d310b73dabf79ad233838eead87792737aa358cf
Alex Zubakin <zubakin.alexandr98@gmail.com>
2023-05-03 18:03:15 +0700

    begin implement view

--- link_representer/views.py
+++ link_representer/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from shorter.models import Url
+
+
+def represent_link_view(request, *args, **kwargs):
+    id = kwargs.get('id')
+    url = Url.objects.get(pk=id)
+
