b46be335d5f3cbd6039fbae34c7a155b47e922b2
Alex Zubakin <zubakin.alexandr98@gmail.com>
2023-05-03 14:06:55 +0700

    add serializer for url

---
+++ shorter/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+
+from .models import Url
+
+
+class UrlSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Url
+        extra_kwargs = {
+            'id': {'read_only': True}
+        }
