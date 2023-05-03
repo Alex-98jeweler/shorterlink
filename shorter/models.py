a800d63d61f101e2558baaeefd9bafac162bfe17
Alex Zubakin <zubakin.alexandr98@gmail.com>
2023-05-03 14:00:27 +0700

    first migrations

--- shorter/models.py
+++ shorter/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
+from .tools import create_short_uuid
 
-
-
-
+class Url(models.Model):
+    id = models.CharField(max_length=8,default=create_short_uuid, primary_key=True, unique=True)
+    long_url = models.URLField()
