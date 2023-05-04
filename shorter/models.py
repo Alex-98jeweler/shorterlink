5906dea773310294355739835bd85e55a15a7f6e
Alex Zubakin <zubakin.alexandr98@gmail.com>
2023-05-04 17:47:01 +0700

    override get absolute url method

--- shorter/models.py
+++ shorter/models.py
@@ -5,3 +5,6 @@ from .tools import create_short_uuid
 class Url(models.Model):
     id = models.CharField(max_length=8,default=create_short_uuid, primary_key=True, unique=True)
     long_url = models.URLField()
+
+    def get_absolute_url(self):
+        return f"/redirect/{self.id}"
